758b139da1d980b85e0513e547b2bbb879986f6a
Laszlo Ori <laszlo.ori@emarsys.com>
2017-03-14 10:46:01 +0100

    Initial commit

---
+++ mobile-engage-sample-app-ios/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mobile-engage-sample-app-ios
+//
+//  Created by Laszlo Ori on 2017. 03. 14..
+//  Copyright (c) 2017 Emarsys. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
