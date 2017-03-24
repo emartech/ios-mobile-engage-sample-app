a049a8f0925923ffc3c2a696b21b0f8ec5255118
Laszlo Ori <laszlo.ori@emarsys.com>
2017-03-24 08:55:10 +0100

    feat(pushwoosh) SUITEDEV-8534 pushwoosh integration

--- Podfile
+++ Podfile
@@ -3,6 +3,7 @@ platform :ios, '9.0'
 target "mobile-engage-sample-app-ios" do
   pod 'MobileEngageSDK', :path => '../ios-mobile-engage-sdk'
   pod 'CoreSDK', :path => '../ios-core-sdk'
+  pod 'Pushwoosh'
 end
 
 target "mobile-engage-sample-app-iosTests" do
--- mobile-engage-sample-app-ios/AppDelegate.swift
+++ mobile-engage-sample-app-ios/AppDelegate.swift
@@ -3,13 +3,22 @@
 //
 
 import UIKit
+import Pushwoosh
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, PushNotificationDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        PushNotificationManager.push().delegate = self
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = PushNotificationManager.push().notificationCenterDelegate
+        }
+        PushNotificationManager.push().handlePushReceived(launchOptions)
+        PushNotificationManager.push().sendAppOpen()
+        PushNotificationManager.push().registerForPushNotifications()
+        
         let config = MEConfig.make { builder in
             builder.setCredentialsWithApplicationId("14C19-A121F", applicationSecret: "PaNkfOD90AVpYimMBuZopCpm8OWCrREu")
         }
@@ -17,35 +26,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-
-    func applicationWillResignActive(_ application: UIApplication) {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        PushNotificationManager.push().handlePushRegistration(deviceToken as Data!)
     }
 
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        PushNotificationManager.push().handlePushRegistrationFailure(error)
     }
 
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        PushNotificationManager.push().handlePushReceived(userInfo)
     }
 
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+//MARK: PushNotificationDelegate
+    func onPushAccepted(_ pushManager: PushNotificationManager!, withNotification pushNotification: [AnyHashable: Any]!, onStart: Bool) {
+        MobileEngage.trackMessageOpen(userInfo: pushNotification)
     }
-
-
-    func applicationWillTerminate(_ application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 
 }
