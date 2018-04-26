68cc44888dd912138f6ad2646ad9f8be5d1625b2
stoto <pstojcsics@emarsys.com>
2018-04-26 16:17:24 +0200

    feat(user-centric-inbox): activate user centric inbox for sample appx
    SUITEDEV-12218
    Co-authored-by: LaszloOriEmarsys <24588073+LaszloOriEmarsys@users.noreply.github.com>

--- mobile-engage-sample-app-ios/AppDelegate.swift
+++ mobile-engage-sample-app-ios/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MEInAppMessageHandler {
         window?.tintColor = UIColor(red: 101 / 255.0, green: 151 / 255.0, blue: 207 / 255.0, alpha: 1.0)
 
         let config = MEConfig.make { builder in
-            builder.setExperimentalFeatures([INAPP_MESSAGING]);
+            builder.setExperimentalFeatures([INAPP_MESSAGING, USER_CENTRIC_INBOX]);
             if let applicationCode = ProcessInfo.processInfo.environment["applicationCode"] as? String,
                let applicationPassword = ProcessInfo.processInfo.environment["applicationPassword"] as? String {
                 builder.setCredentialsWithApplicationCode(applicationCode, applicationPassword: applicationPassword)
